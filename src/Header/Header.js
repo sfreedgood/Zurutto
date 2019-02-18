bbb6d49cfe04ef602bbd531c2505e62d2ed448a0
Christina Sopova <work.sopova@gmail.com>
2019-02-17 21:45:35 -0500

    header, footer, about

---
+++ src/Header/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Header.css";
+import logo from "../assets/logo.png";
+//we have to assign ids to menu and contact component
+const Header = () => {
+  return (
+    <div className="header">
+      <a href="#menu">Menu</a>
+      <img src={logo} alt="logo" />
+      <a href="#contact">Contact</a>
+    </div>
+  );
+};
+
+export default Header;
