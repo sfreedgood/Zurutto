604a6cb36784793391c9c6e324410589a8be7a5a
Charlie Sheppard <sheppard.charlie24@gmail.com>
2019-02-18 22:22:08 -0500

    add content for splash page with image and scroll arrow

--- src/splash/Splash.js
+++ src/splash/Splash.js
@@ -1,11 +1,15 @@
 import React from "react"
+import './Splash.css'
+import SplashImg from './splash.png'
+import arrow from './arrow.png'
 
 const Splash = () => {
 
 
     return(
         <div className="splash">
-            Splash Page Test
+            <img src={SplashImg} className='splash-img' />
+            <img src={arrow} className='scroll-arrow' />
         </div>
     )
 }
